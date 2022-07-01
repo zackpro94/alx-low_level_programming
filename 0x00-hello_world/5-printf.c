983c5c94c1a17a79b35af077db5701c7ea7519ac
zack <zekarias94@gmail.com>
2022-07-01 04:05:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * return: if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
