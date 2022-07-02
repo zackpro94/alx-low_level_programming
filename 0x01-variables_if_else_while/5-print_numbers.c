094302a14c5c3950e11c73e20701c89e6f84a9a6
zack <zekarias94@gmail.com>
2022-07-02 02:32:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single numbers 1 - 10
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
