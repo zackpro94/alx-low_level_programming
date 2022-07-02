f03ebcae74cb20dcd5ebdb12548c836cbe25af8d
zack <zekarias94@gmail.com>
2022-07-02 01:27:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower case
+ * Return: 0 succrss
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'a'; z <= 'z'; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
