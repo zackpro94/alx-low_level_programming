6d5d726adef801e8bb1bba1433a07ff67db446ba
zack <zekarias94@gmail.com>
2022-07-05 13:47:31 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_Putchar((k % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
