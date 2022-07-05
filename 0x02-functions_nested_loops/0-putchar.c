a1eb7b405037cd4a6abffc82ba669f2031d7acb4
zack <zekarias94@gmail.com>
2022-07-05 09:37:44 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * Return: success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	-putchar('a');
+	_putchar('r');
+
+	return (0);
+}
